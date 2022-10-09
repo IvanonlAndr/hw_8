18588e3530270783f1084425889c4f7674ffc7c3
IvanonlAndr <96746263+IvanonlAndr@users.noreply.github.com>
2022-10-09 15:22:18 +0200

    Add files via upload

---
+++ js/gallery.js
@@ -0,0 +1,6 @@
+$('.header__menu-btn').on('click', function () {
+    $('.header__menu-btn').toggleClass('header__menu-btn_close');
+  });
+  $('.header__menu-btn').on('click', function () {
+    $('.header__inner-items').toggleClass('header__inner-items-active');
+  });
